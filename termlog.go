42392f455dfc1f86ad4fe442a222f0240c0e1ddb
guanwei <guanwei@lianjia.com>
2017-03-01 16:15:54 +0800

    modify term log format

--- termlog.go
+++ termlog.go
@@ -20,7 +20,7 @@ type ConsoleLogWriter struct {
 // This creates a new ConsoleLogWriter
 func NewConsoleLogWriter() *ConsoleLogWriter {
 	consoleWriter := &ConsoleLogWriter{
-		format: "[%T %D] [%L] (%S) %M",
+		format: "[%D %T] [%L] (%S) %M",
 		w:      make(chan *LogRecord, LogBufferLength),
 	}
 	go consoleWriter.run(stdout)
